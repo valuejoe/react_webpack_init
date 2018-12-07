e68ac7f767c8496cf8b734226f283df1927ae52c
Joe <chiaohung82@gmail.com>
2018-12-07 12:33:11 +0800

    init webpack

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+ReactDOM.render( 
+	<div>hello world</div>,
+	document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: [
+    './src/js/index.js',
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/index.css",
+      chunkFilename: "./index.css"
+    })
+  ]
+};
